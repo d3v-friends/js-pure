22f93f7e2f37873500cae474860735f7961b2d64
CiaoLee <ciaolee87@gmail.com>
2023-12-15 15:36:03 +0900

    1. save

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf cjs
 rm -rf esm
 tsc -p tsconfig.cjs.json
 tsc -p tsconfig.esm.json
-find ./cjs -type f -name '*.js' -exec rename 's/\.js/\.cjs/' '{}' \;
+find ./esm -type f -name '*.js' -exec rename 's/\.js/\.mjs/' '{}' \;
 
 git add cjs --all
 git add esm --all
