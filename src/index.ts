6fc84cbf1ff7a08e25d7babedbd0a248729962dc
CiaoLee <ciaolee87@gmail.com>
2023-12-15 10:19:55 +0900

    1. delete node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import fnEnv from "./fnEnv";
-import fnParam from "./fnParam";
-import fnPath from "./fnPath";
-import { JsError } from "./fnError/jsError";
 
-export type Bool = "true" | "false";
-export default {
-    fnEnv,
-    fnParam,
-    fnPath,
-    JsError,
-};
+declare namespace jsPure {
+
+}
+
