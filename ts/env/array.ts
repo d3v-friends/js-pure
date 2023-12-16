373c1c122055673c6a217e2679b06cb721d7de56
CiaoLee <ciaolee87@gmail.com>
2023-12-16 22:45:41 +0900

    1. add fnEnv.array()

---
+++ ts/env/array.ts
@@ -0,0 +1,16 @@
+import * as process from "process";
+
+function array(key: string): string[] {
+    let str = process.env[key] || "";
+    for (let i = 0; i < str.lastIndexOf(" "); i++) {
+        str = str.replace(" ", "");
+    }
+
+    if (!str) return [];
+    if (!str.startsWith("[")) return [];
+    if (!str.endsWith("]")) return [];
+    str = str.slice(1, str.length - 1);
+    return str.split(",");
+}
+
+export default array;
--- ts/env/index.ts
+++ ts/env/index.ts
@@ -1,9 +1,11 @@
 import boolean from "./boolean";
 import read from "./read";
 import string from "./string";
+import array from "./array";
 
 export default {
     string,
     boolean,
     read,
+    array,
 };
