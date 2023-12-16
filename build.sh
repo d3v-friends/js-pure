8514568847b80be923c92eeef18137a459c64182
CiaoLee <ciaolee87@gmail.com>
2023-12-16 11:47:03 +0900

    1. save

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 set -e
 
 rm -rf cjs
-rm -rf mjs
-tsc -p tsconfig.cjs.json
-tsc -p tsconfig.mjs.json
+echo ">> tsc cjs"
+tsc -p tsconfig.json
 find ./cjs -type f -name '*.js' -exec rename 's/\.js/\.cjs/' '{}' \;
-find ./mjs -type f -name '*.js' -exec rename 's/\.js/\.mjs/' '{}' \;
 git add cjs --all
-git add mjs --all
--- mjs/env/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare function string(key: string, ...defaultValue: string[]): string;
-declare function boolean(key: string, ...defaults: boolean[]): boolean;
-declare function readFile(...str: string[]): Promise<void>;
-declare const fnEnv: {
-    string: typeof string;
-    boolean: typeof boolean;
-    readFile: typeof readFile;
-};
-export default fnEnv;
