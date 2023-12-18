509366119801f292aa71f62ad048eb7abae572f7
xcode <xcode@stdlib.in>
2023-12-18 17:49:09 +0900

    1. save

--- index.ts
+++ index.ts
@@ -1,33 +1,4 @@
-// type
 export { Nullable } from "@type/index";
-
-// fnErr
-import Error from "@err/jsError";
-import getMsg from "@err/getMsg";
-
-export const fnErr = {
-    Error,
-    getMsg,
-};
-
-// env
-import fnEnvString from "@env/string";
-import fnEnvArray from "@env/array";
-import fnEnvRead from "@env/read";
-import fnEnvBoolean from "@env/boolean";
-
-export const fnEnv = {
-    string: fnEnvString,
-    array: fnEnvArray,
-    read: fnEnvRead,
-    boolean: fnEnvBoolean,
-};
-
-// fnParam
-import fnParamString from "@param/string";
-import fnParamBoolean from "@param/boolean";
-
-export const fnParam = {
-    string: fnParamString,
-    boolean: fnParamBoolean,
-};
+export * as fnError from "./err";
+export * as fnEnv from "./env";
+export * as fnParam from "./param";
--- test/test.ts
+++
@@ -1,6 +0,0 @@
-// import { jsPure } from "@src/index";
-//
-// fnEnv.string("");
-// fnParam.string([]);
-//
-// const err = new fnErr.Error(fnErr.msg("", {}), { ko: "" });
